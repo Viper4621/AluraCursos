77b0214f1727a5b8231417b73864012827993a28
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-13 13:51:19 -0300

    Logica de numero finalizada

--- logica de programação/app.js
+++ logica de programação/app.js
@@ -1,12 +1,13 @@
 alert("Boas vindas ao jogo do numero secreto");
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 console.log(numeroSecreto);
 
 while (chute != numeroSecreto) {
-  chute = prompt("Digite um numero entre 1 e 100");
+  chute = prompt(`Digite um numero entre 1 e ${numeroMaximo}`);
 
   if (chute == numeroSecreto) {
     break;
