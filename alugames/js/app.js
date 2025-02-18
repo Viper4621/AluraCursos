eb633c9d19cf82d35327e0cbf465b8305664aae2
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-18 20:51:01 -0300

    Selecionando ids para modificações no dom

---
+++ alugames/js/app.js
@@ -0,0 +1,8 @@
+function alterarStatus(id) {
+  let gameClicado = document.getElementById(`game-${id}`);
+  let imagem = document.gameClicado.querySelector(".dashboard__item__img");
+  let botao = document.gameClicado.querySelector("dashboard__item__button");
+  let nomeJogo = document.gameClicado.querySelector("dashboard__item__name");
+
+  alert(nomeJogo.textContent);
+}
