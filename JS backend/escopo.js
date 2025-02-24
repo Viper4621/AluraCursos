281e630204a552b949d0e0d56da0e09177a0dddd
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-24 20:47:50 -0300

    Referente a escopo

---
+++ JS backend/escopo.js
@@ -0,0 +1,11 @@
+const estudante = "Erick";
+
+if (1 > 0) {
+  //   estudante = "Erick";
+  console.log(estudante);
+}
+//uma var criada do lado de fora pode mudar o conteudo de uma dentro de um escopo
+// abaixo criamos var com mesmo nome porem não se refere a mesma informação
+//ctrl l limpar console
+
+console.log(estudante);
