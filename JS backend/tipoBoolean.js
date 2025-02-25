eb733c69cb13e89cc4b412b25bd927dc13c93eb3
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-25 14:47:21 -0300

    Tipo boolean

---
+++ JS backend/tipoBoolean.js
@@ -0,0 +1,14 @@
+const estudante = "jhony";
+const estaAprovado = true;
+
+if (estaAprovado) {
+  console.log("parabens boas festas");
+} else {
+  console.log("reprovado boas festas");
+}
+
+if (estudante === "jhony") {
+  console.log(`ola, ${estudante}`);
+} else {
+  console.log("quem é você?");
+}
