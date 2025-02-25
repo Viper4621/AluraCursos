31fab4948d51244a7131ada6ec3885fd6a752557
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-25 14:36:15 -0300

    TipoStrings

---
+++ JS backend/tipoString.js
@@ -0,0 +1,20 @@
+const estudante = "erick";
+const docente = "luciano";
+const cumprimento = "nosso lema é 'Estudar Bastante!' ";
+const citacao = `Ju diz: ${cumprimento}`;
+
+console.log(cumprimento);
+console.log(citacao);
+
+console.log(`o estudante se chama ${estudante}`);
+
+const senha = "senhaSegura123" + estudante.toUpperCase();
+console.log(senha);
+
+let valor1 = 0;
+let abacate;
+let valor2 = "10";
+
+console.log("valor1 é do tipo:", typeof valor1);
+console.log("abacate é do tipo:", typeof abacate);
+console.log("valor2 é do tipo:", typeof valor2);
