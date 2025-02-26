c0c862fc527779f64650d0c32c8f974f5558c34b
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-26 20:47:58 -0300

    operadoresComparação

---
+++ JS backend/operadoresCompacao.js
@@ -0,0 +1,11 @@
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+  console.log("Aprovado");
+}
+// == são iguais === são iguais e do mesmo tipo vai dar erro pois um é string outro numero
+if ("0" == 0) {
+  console.log("passou na comparação");
+} else {
+  console.log("não passou");
+}
