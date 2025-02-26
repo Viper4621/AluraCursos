29668bf1ca7197f918c45a26b63a757d51440fea
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-02-26 20:44:03 -0300

    truth e falsy

--- JS backend/operadoresLogicos.js
+++ JS backend/operadoresLogicos.js
@@ -1,5 +1,6 @@
 const notaFinal = 7;
 const faltas = 3;
+const advertencias = 0;
 
 //aqui é se nota final menor que 7 ou faltas > 4 = resultado reprovado
 // if (notaFinal < 7 || faltas > 4) {
@@ -12,4 +13,11 @@ if (notaFinal < 7 && faltas > 4) {
   console.log("Reprovado, Boas festas");
 } else {
   console.log("Aprovado, Boas festas");
+}
+//truthy e false caso abaixo queremos que não tenha advertencias como a const tem valor 0 e usamos!advertencias
+//vai gerar true e usando && queremos 2 true para acessar o bloco de codigo sendo assim funcionando ok recebeu bonus
+if (faltas >= 2 && !advertencias) {
+  console.log("recebeu bonus");
+} else {
+  console.log("nao recebeu bonus");
 }
