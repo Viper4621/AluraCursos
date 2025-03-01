e8b01252d00e73c1c8d81e7792c51a0c57bab20b
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-01 08:32:06 -0300

    Listas

---
+++ JS backend arrays/calculoMedia.js
@@ -0,0 +1,9 @@
+// const nota1 = 10;
+// const nota2 = 7;
+// const nota3 = 5;
+// const nota4 = 3;
+
+const notas = [10, 8, 7.5, 5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media.toFixed(2));
