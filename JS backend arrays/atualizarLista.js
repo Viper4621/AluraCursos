147188f88a89960117be5879c2d3bbe63dfc9b0e
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-01 10:37:23 -0300

    splice()

---
+++ JS backend arrays/atualizarLista.js
@@ -0,0 +1,7 @@
+const listaEstudantes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+//retirar itens e adicionar ao mesmo tempo metodo splice()
+// o 1 parametro é o indice o 2 quantidade 3 parametro e algo que queremos incluir onde retiramos
+listaEstudantes.splice(1, 2, "rodrigo");
+
+console.log(listaEstudantes);
