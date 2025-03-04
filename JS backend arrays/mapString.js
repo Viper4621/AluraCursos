c0347f18619d652061d2b70b95c7079449ee4272
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-04 09:44:48 -0300

    map() para string alteração

---
+++ JS backend arrays/mapString.js
@@ -0,0 +1,6 @@
+//map pode fazer qualquer tipo de alteração no caso modificamos para UpperCase
+const nomes = ["ana julia", "Caio Vinicios", "Bia silva"];
+
+const padronizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(padronizados);
