3675431f9db8af8b6e42e6d4bf65b3463092df36
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-04 09:50:35 -0300

    Atualizando com exercicios

--- JS backend arrays/mapString.js
+++ JS backend arrays/mapString.js
@@ -4,3 +4,20 @@ const nomes = ["ana julia", "Caio Vinicios", "Bia silva"];
 const padronizados = nomes.map((nome) => nome.toUpperCase());
 
 console.log(padronizados);
+
+const nome = "Alura";
+
+let letrasMaisculas = "";
+
+for (let i = 0; i < nome.length; i++) {
+  letrasMaisculas += nome[i].toUpperCase();
+}
+
+console.log(letrasMaisculas);
+
+const nomeDoCurso = "Fundamentos em JS";
+const nomePlataforma = " Alura";
+
+const completa = nomeDoCurso.concat(nomePlataforma).toUpperCase();
+
+console.log(completa);
