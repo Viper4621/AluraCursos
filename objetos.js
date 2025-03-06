e44b78004228ea0a125e18669dd3fcd64cc234ca
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-06 15:37:42 -0300

    Objetos

---
+++ objetos.js
@@ -0,0 +1,13 @@
+const listaCPFs = ["12312312312", "123234435345"];
+
+const infosPessoa = ["nome", "José Silva", "idade", 32];
+
+console.log(infosPessoa[1]);
+
+//estrutura de obj funciona quando precisamos guardar muito mais dados de algum objeto
+const objPessoa = {
+  nome: "José Silva",
+  idade: 32,
+  cpf: "12312312345",
+  turma: "JavaScript",
+};
