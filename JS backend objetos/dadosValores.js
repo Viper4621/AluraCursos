732cae6b31e2f5effe3e07f82971bc6eafb97762
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-07 11:47:40 -0300

    Transformando propriedade string em array

---
+++ JS backend objetos/dadosValores.js
@@ -0,0 +1,11 @@
+const estudante = {
+  nome: "José Silva",
+  idade: 32,
+  cpf: "12312312345",
+  turma: "JavaScript",
+  bolsista: true,
+  //transformando propriedade string em array com 2 elementos
+  telefones: ["55119999911", "55119999911"],
+};
+//podemos usar posição para acessar tambem
+console.log(estudante.telefones[1]);
