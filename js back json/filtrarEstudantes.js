34b3ffe06bbae424f888c9fa0350fc088e83ed97
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-13 14:56:06 -0300

    att

--- js back json/filtrarEstudantes.js
+++ js back json/filtrarEstudantes.js
@@ -10,3 +10,16 @@ function filtrarPorPropriedade(lista, propriedade) {
 
 const listaEnderecosIncompletos = filtrarPorPropriedade(estudantes, "cep");
 console.log(listaEnderecosIncompletos);
+
+const listaDeUsuarios = [
+  { id: 1, nome: "Alice", idade: 25 },
+  { id: 2, nome: "Bob", idade: 30 },
+  { id: 3, nome: "Charlie", idade: 22 },
+];
+
+function encontrarUsuario(id) {
+  return listaDeUsuarios.find((usuario) => usuario.id === id);
+}
+
+const usuarioBuscado = encontrarUsuario(3);
+console.log(usuarioBuscado);
