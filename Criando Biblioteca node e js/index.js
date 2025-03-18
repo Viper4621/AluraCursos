f24bf51005afdbfc5315b178982539f4ecd1af79
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-18 10:44:40 -0300

    Atualização de documentação

--- Criando Biblioteca node e js/index.js
+++ Criando Biblioteca node e js/index.js
@@ -9,9 +9,14 @@ const fs = require("fs");
 //process.argv da instruções para o terminal que vai deixar tudo dentro de array
 //podemos pegar os 3 links e passar para ele
 const caminhoArquivo = process.argv;
+//porque estamos colocando a posição [2]?
+//process.argv cria o array que vai receber [array com info do node index[0],caminho dos arquivos[1] e agora o que desejamos ler[2]]
 const link = caminhoArquivo[2];
 
 //utf-8 encoding de nossos caracteres para saber como interpretar
+//nos parametros colocamos a const que usamos para salvar o link + formato encoder e passamos 2 função callback erro ou ok=texto
 fs.readFile(link, "utf-8", (erro, texto) => {
   console.log(texto);
 });
+
+console.log(caminhoArquivo);
