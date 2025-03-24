bd9836dcfd3beec758db58c436fb48d814e77f1c
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-24 19:28:41 -0300

    poo

---
+++ JS orientação a objetos/objetoLiteral.js
@@ -0,0 +1,9 @@
+const user = {
+  nome: "erick",
+  email: "cdzbr46211@gmail.com",
+  nascimento: "06/06/1988",
+  role: "estudante",
+  exibirInfors: function () {
+    console.log(this.nome, this.email);
+  },
+};
