93b3289fefd86b46322f6c468a7728cec90f0b4e
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-03-29 19:08:44 -0300

    usando call e this

--- JS orientação a objetos/new.js
+++ JS orientação a objetos/new.js
@@ -12,3 +12,63 @@ function User(nome, email) {
 //com isso ira retornar um usuario novo com os parametros de nome e email que criamos seguindo o this e trazendo o return de mostrar
 const novoUser = new User("Erick", "cdzbr46211@gmail.com");
 console.log(novoUser.exibirInfos());
+
+//exemplo de utilização com call para chamar dados de uma const usando this de uma função
+function exibirInfos1() {
+  console.log(this.nome, this.email);
+}
+
+const user = {
+  nome: "Mariana",
+  email: "m@m.com",
+};
+
+exibirInfos1.call(user);
+
+//outro exemplo de utilização de call
+//uma função depois um objeto com função dentro dele podemos acessar utilizando uma função com call
+function exibeMensagem(nome, email) {
+  console.log(`Seu nome é ${nome}, seu email ${email}`);
+}
+
+const usuario = {
+  nome: "alguem",
+  email: "algum@gmail.com",
+  executaFuncao: function (fn) {
+    fn.call(user, this.nome, this.email);
+  },
+};
+
+usuario.executaFuncao(exibeMensagem);
+
+//outro exemplo
+const userDefault = {
+  exibirInfos: function () {
+    console.log(this.nome, this.email);
+  },
+  cadastraEmail: function (email) {
+    console.log(`email ${email} cadastrado com sucesso`);
+  },
+};
+
+const atendente = {
+  nome: "Rodrigo",
+  email: "r@r.com",
+  role: "atendente",
+};
+
+const gerente = {
+  nome: "Juliana",
+  email: "j@j.com",
+  role: "gerente sênior",
+  removeUsuario: function (idUsuario) {
+    console.log(`usuário ${idUsuario} removido`);
+  },
+};
+
+Object.setPrototypeOf(atendente, userDefault);
+Object.setPrototypeOf(gerente, userDefault);
+
+gerente.removeUsuario(123);
+atendente.exibirInfos();
+atendente.cadastraEmail(this.email);
