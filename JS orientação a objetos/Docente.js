a1762524fbececd22c6635ce2a8cc1cc515c5c30
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-04-01 10:31:45 -0300

    Hernça + método próprio em class

---
+++ JS orientação a objetos/Docente.js
@@ -0,0 +1,13 @@
+import User from "./User.js";
+
+class Docente extends User {
+  constructor(nome, email, nascimento, role = "Docente", ativo = "true") {
+    super(nome, email, nascimento, role, ativo);
+  }
+  aprovarEstudante(estudante, curso) {
+    return `o estudante: ${estudante}, foi aprovado no curso: ${curso}, o responsavel ${this.nome}`;
+  }
+}
+
+const novaDocente = new Docente("Ana", "ana.pereira@gmail.com", "05/05/1980");
+console.log(novaDocente.aprovarEstudante("Juliana", "javascript"));
