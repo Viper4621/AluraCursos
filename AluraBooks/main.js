51c1df500f63082ce038926c059566b84cbfcdc2
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-04-23 14:30:11 -0300

    Criando metodo para desconto

--- AluraBooks/main.js
+++ AluraBooks/main.js
@@ -2,28 +2,10 @@ let livros = [];
 const endpointDaAPI =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 getBuscarLivrosDaAPI();
-const elementoParaInserirLivros = document.getElementById("livros");
 
 async function getBuscarLivrosDaAPI() {
   const res = await fetch(endpointDaAPI);
   livros = await res.json();
-  console.log(livros);
-  exibirLivrosNaTela(livros);
-}
-
-function exibirLivrosNaTela(ListaDeLivros) {
-  ListaDeLivros.forEach((livro) => {
-    elementoParaInserirLivros.innerHTML += `<div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}"
-        alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>`;
-  });
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirLivrosNaTela(livrosComDesconto);
 }
