75b047819105bffd8d6b2a56d80346cf19025f6d
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-04-24 17:06:33 -0300

    value no index e operador ternario de value

--- AluraBooks/metodoFilter.js
+++ AluraBooks/metodoFilter.js
@@ -4,6 +4,9 @@ botoes.forEach((btn) => btn.addEventListener("click", FiltrarLivros));
 function FiltrarLivros() {
   const elementoBtn = document.getElementById(this.id);
   const categoria = elementoBtn.value;
-  let livrosFiltrados = livros.filter((livro) => livro.categoria == categoria);
-  console.table(livrosFiltrados);
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? livros.filter((livro) => livro.quantidade > 0)
+      : livros.filter((livro) => livro.categoria == categoria);
+  exibirLivrosNaTela(livrosFiltrados);
 }
