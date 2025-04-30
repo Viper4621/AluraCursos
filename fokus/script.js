15ddc8829cbc2a4c53be3f32caf76bcd074bc842
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-04-30 13:43:23 -0300

    Alterando icones play pause

--- fokus/script.js
+++ fokus/script.js
@@ -7,8 +7,16 @@ const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
 const startPauseBt = document.querySelector("#start-pause");
 const musicaFocoInput = document.querySelector("#alternar-musica");
+const iniciarOuPausarBt = document.querySelector("#start-pause span");
+const iniciarOuPausarBtIcone = document.querySelector(
+  ".app__card-primary-butto-icon"
+);
 const musica = new Audio("/sons/luna-rise-part-one.mp3");
 musica.loop = true;
+const audioPlay = new Audio("/sons/play.wav");
+const audioPausa = new Audio("/sons/pause.mp3");
+const audioTempoFinalizado = new Audio("/sons/beep.mp3");
+const tempoNaTela = document.querySelector("#timer");
 
 let tempoDecorridoEmSegundos = 5;
 let intervaloId = null;
@@ -62,25 +70,39 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
-    zerar();
+    audioTempoFinalizado.play();
     alert("tempo finalizado");
+    zerar();
     return;
   }
   tempoDecorridoEmSegundos -= 1;
-  console.log("temporizador: " + tempoDecorridoEmSegundos);
+  mostrarTempo();
 };
 
 startPauseBt.addEventListener("click", iniciarOuPausar);
 
 function iniciarOuPausar() {
   if (intervaloId) {
+    audioPausa.play();
     zerar();
     return;
   }
+  audioPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.textContent = "Pausar";
+  iniciarOuPausarBtIcone.setAttribute("src", "/imagens/pause.png");
 }
 
 function zerar() {
   clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = "Começar";
+  iniciarOuPausarBtIcone.setAttribute("src", "/imagens/play_arrow.png");
   intervaloId = null;
 }
+
+function mostrarTempo() {
+  const tempo = tempoDecorridoEmSegundos;
+  tempoNaTela.innerHTML = `${tempo}`;
+}
+
+mostrarTempo();
