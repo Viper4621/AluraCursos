c6e166a77ce9a05c031785bfdeae0739016071a3
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-05-07 10:34:30 -0300

    criando função de submit
    criamos uma array para salvar tarefas / armazenamos a tarefa do textarea em uma variavel e demos push para nosso array de tarefas
    e por fim estamos salvando no localStorage uma chave de tarefa para pegar nossa tarefa

--- fokus att/script-crud.js
+++ fokus att/script-crud.js
@@ -1,6 +1,22 @@
 //encontrar btn add tarefa
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textArea = document.querySelector(".app__form-textarea");
+
+const tarefas = [];
+
 btnAdicionarTarefa.addEventListener("click", () => {
   formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  //criamos um objeto para pegar o valor da text area e salvar // criamos o array de tarefas acima
+  const tarefa = {
+    descricao: textArea.value,
+  };
+  //estmaos jogando essa tarefa dentro de nosso array tarefas
+  tarefas.push(tarefa);
+  //estamos criando no local uma chave de acesso que ira armazenar nosso array de tarefas 
+  localStorage.setItem("tarefas", tarefas);
 });
