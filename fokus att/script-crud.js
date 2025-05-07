e2b02e432a71dd06a77cb0e997ffb773ad1fecf1
Erick Luciano Caetano da Silva <cdzbr46211@gmail.com>
2025-05-07 10:23:39 -0300

    Projeto fokus
    iniciando projeto e criando os seletores para botao de adicionar e mudar classlist de formulario para ficar visivel

---
+++ fokus att/script-crud.js
@@ -0,0 +1,6 @@
+//encontrar btn add tarefa
+const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
+const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+btnAdicionarTarefa.addEventListener("click", () => {
+  formAdicionarTarefa.classList.toggle("hidden");
+});
